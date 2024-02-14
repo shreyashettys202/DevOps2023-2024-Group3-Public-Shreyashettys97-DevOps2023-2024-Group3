c5f9466fe680ad72bae83a6342d1999302262130
shreyashettys202 <shreyashettys202@gmail.com>
2024-02-14 18:14:29 +0000

    DGW-105 UI code changes for App.js

--- WWA-frontend-master/src/App.js
+++ WWA-frontend-master/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AdminPage from "./pages/AdminPage";
 import ViewBookingsPage from "./pages/ViewBookingsPage";
+import AdminBookingsPage from "./pages/AdminBookingsPage";
 import Errorpage from "./pages/ErrorPage";
 
 
@@ -13,7 +15,9 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<HomePage />}></Route>
+          <Route path="/admin" element={<AdminPage />}></Route>
           <Route path="/bookings" element={<ViewBookingsPage />}></Route>
+          <Route path="/admin/bookings" element={<AdminBookingsPage />}></Route>
           <Route path="*" element={<Errorpage/>}></Route>
         </Routes>
       </Router>
