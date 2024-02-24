2e5c0ff760b12978a4c927558c61e156482f165b
pavan kalyan <p.kalyanvarikolu@gmail.com>
2024-02-25 00:20:50 +0530

    adding all the shell scripts to install docker, minikube and prometheus on ubuntu

---
+++ scripts/minikube.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+sudo su ${USER}
+groups
+# add current user to docker group so there is no need to use sudo when running docker
+sudo chmod 666 /var/run/docker.sock
+# here installation of minikube starts
+# ensure that docker is running
+sudo systemctl start docker
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
+sudo install minikube-latest.x86_64.rpm /usr/local/bin/minikube
+# Verify the installation
+minikube version
+# Start Minikube with the Docker driver
+minikube start --driver=docker
